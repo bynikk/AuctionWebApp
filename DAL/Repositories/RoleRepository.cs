292f5add3b0f4a80a9dbeef4f15b68b0e4830141
Nikita Prachkovsky <bynik2001@gmail.com>
2022-04-14 00:36:56 +0300

    Add role/user/auctionitemrepositories

---
+++ DAL/Repositories/RoleRepository.cs
@@ -0,0 +1,38 @@
+﻿using BLL.Entities;
+using BLL.Interfaces;
+using MongoDB.Driver;
+
+namespace DAL.Repositories
+{
+    internal class RoleRepository : IRepository<Role>
+    {
+        IDbContext context;
+        public RoleRepository(IDbContext context)
+        {
+            this.context = context;
+        }
+        public Task Create(Role item)
+        {
+            return context.Roles.InsertOneAsync(item);
+        }
+
+        public Task Delete(int id)
+        {
+            return context.Roles.DeleteOneAsync(c => c.UserId == id);
+        }
+
+        public Task<List<Role>> GetAll()
+        {
+            return context.Roles.Find(_ => true).ToListAsync(); ;
+        }
+
+        public Task Update(Role item)
+        {
+            var filter = Builders<Role>.Filter.Eq("UserId", item.UserId);
+            var update = Builders<Role>.Update
+                                          .Set(x => x.Name, item.Name);
+
+            return context.Roles.UpdateOneAsync(filter, update);
+        }
+    }
+}
